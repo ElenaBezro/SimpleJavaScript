10f2a3133976c7e0ab52a83844539a18859306bf
Elena Bezrodnova <elenabezrodnova@Elenas-Air.fritz.box>
2023-09-29 12:50:48 +0200

    changed build file

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 
 javac plusOne.java
 
-jar --create --file plusOne.jar --manifest=META-INF/MANIFEST.MF plusOne.class
+jar --create --file plusOne.jar --manifest=MANIFEST.MF plusOne.class
 
 java -jar ./plusOne.jar
